c47453063602edad46a4c0e4118c9ac070d089ad
Jeff-Tian <Jeff-Tian@users.noreply.github.com>
2022-09-18 12:01:44 +0000

    deploy: bc2fd84977e5d660fd867443daf2c4be112bdf5d

---
+++ ApiTest/WecomTokenTest.cs
@@ -0,0 +1,27 @@
+using System.Net;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace ApiTest
+{
+    [TestClass]
+    public class WecomTokenTest
+    {
+        [TestMethod]
+        public async Task TestGetTokenOk()
+        {
+            var res = await client.GetAsync("/api/wecom/token/hardway");
+            Assert.AreEqual(HttpStatusCode.OK, res.StatusCode);
+
+            var body = await res.Content.ReadAsStringAsync();
+            Assert.AreEqual("", body);
+        }
+
+        HttpClient client;
+        [TestInitialize]
+        public void Setup() {
+            var application = new WebApplicationFactory<Program>().WithWebHostBuilder(builder=>{});
+
+            client = application.CreateClient();
+        }
+    }
+}
