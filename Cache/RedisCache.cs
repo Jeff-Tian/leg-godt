dc50d6a523ef03a63ee19f2b19cfad0d405e41c3
Jeff Tian@Workstation <jie.tian@live.cn>
2022-09-24 19:26:24 +0800

    feat: add inmemory cache

---
+++ Cache/RedisCache.cs
@@ -0,0 +1,23 @@
+﻿using StackExchange.Redis;
+
+namespace Cache;
+
+public class RedisCache : CacheInstance
+{
+    public bool Set(string key, string value)
+    {
+        var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+        if (env is null)
+        {
+            Environment.SetEnvironmentVariable("REDIS_HOST", "localhost");
+            Environment.SetEnvironmentVariable("REDIS_PASSWORD", "eYVX7EwVmmxKPCDmwMtyKVge8oLd2t81");
+        }
+        
+        var redisConnectionString = $"{Environment.GetEnvironmentVariable("REDIS_HOST")},password={Environment.GetEnvironmentVariable("REDIS_PASSWORD")}";
+        
+        var redis = ConnectionMultiplexer.Connect(redisConnectionString);
+
+        var db = redis.GetDatabase();
+        return db.StringSet(key, value);
+    }
+}
