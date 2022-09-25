e042f0d2f91bb5385f51434d836ff1972e55dd0d
cnjeftia <jeff.tian@outlook.com>
2022-09-25 15:26:42 +0800

    test: mock corp

--- ApiTest/WecomTokenTest.cs
+++ ApiTest/WecomTokenTest.cs
@@ -1,5 +1,10 @@
+using System.Diagnostics;
 using System.Net;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using Store;
+using Web.Models;
 
 namespace ApiTest
 {
@@ -10,14 +15,27 @@ namespace ApiTest
         public async Task TestGetTokenOk()
         {
             var res = await client.GetAsync("/api/wecom/token/hardway");
-            Assert.AreEqual(HttpStatusCode.InternalServerError, res.StatusCode);
+            Assert.AreEqual(HttpStatusCode.OK, res.StatusCode);
         }
 
         HttpClient client;
+
         [TestInitialize]
-        public void Setup() {
+        public void Setup()
+        {
             Environment.SetEnvironmentVariable("ENV", "test");
-            var application = new WebApplicationFactory<Program>().WithWebHostBuilder(builder=>{});
+            var application = new WebApplicationFactory<Program>().WithWebHostBuilder(builder =>
+            {
+            });
+
+            var scope = application.Services.CreateScope();
+            var wecomContext = scope.ServiceProvider.GetService<WecomCorpContext>();
+            var fakeCorp = Corporation.GetFakeCorp();
+
+            Debug.Assert(wecomContext != null, nameof(wecomContext) + " != null");
+
+            wecomContext.WecomCorps.Add(fakeCorp);
+            wecomContext.SaveChanges();
 
             client = application.CreateClient();
         }
