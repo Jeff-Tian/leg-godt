64b4b2d653d7198261d73cb7a66733e27116f1a6
Jeff Tian <jeff.tian@outlook.com>
2023-12-28 18:48:54 +0800

    feat(UNI-175): use ses

--- ApiTest/MailCommandTest.cs
+++ ApiTest/MailCommandTest.cs
@@ -1,6 +1,11 @@
 using System.Net;
 using System.Net.Http.Json;
+using Amazon.SimpleEmail;
+using ApiTest.Fixtures;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 using Web.Controllers.Mail;
 
 namespace ApiTest;
@@ -8,7 +13,7 @@ namespace ApiTest;
 [TestClass]
 public class MailCommandTest
 {
-    HttpClient? client;
+    private HttpClient? _client;
 
     [TestInitialize]
     public void Setup()
@@ -16,16 +21,21 @@ public class MailCommandTest
         Environment.SetEnvironmentVariable("ENV", "test");
         var application = new WebApplicationFactory<Program>().WithWebHostBuilder(builder =>
         {
+            builder.ConfigureTestServices(services =>
+            {
+                services.RemoveAll<IAmazonSimpleEmailService>();
+                services.AddSingleton<IAmazonSimpleEmailService>(new MockEmailClient());
+            });
         });
 
-        client = application.CreateClient();
+        _client = application.CreateClient();
     }
 
     [TestMethod]
     public async Task TestSendEmailOk()
     {
         var mailCommand = new MailCommand("jeff.tian@outlook.com", "hello", "world!");
-        var response = await client?.PostAsync("/api/Mail/SendEmail", JsonContent.Create(mailCommand))!;
+        var response = await _client?.PostAsync("/api/Mail/SendEmail", JsonContent.Create(mailCommand))!;
         Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
         var body = await response.Content.ReadAsStringAsync();
         Assert.AreEqual("Success", body);
