190612d55a4c783554d4164ce8e52b4389c34fce
Jeff Tian <jeff.tian@outlook.com>
2023-12-28 19:46:01 +0800

    test(UNI-175): fix test

--- WebApiTest/WeComTokenTest.cs
+++ WebApiTest/WeComTokenTest.cs
@@ -11,6 +11,8 @@ namespace WebApiTest
         public async Task TestGetTokenOk()
         {
             Environment.SetEnvironmentVariable("ENV", "test");
+            Environment.SetEnvironmentVariable("AWS_ACCESS_KEY_ID", "fake");
+            Environment.SetEnvironmentVariable("AWS_SECRET_ACCESS_KEY", "fake");
             var application = new WebApplicationFactory<Program>()
                 .WithWebHostBuilder(builder =>
                 {
