f297c960fbb1a801e7acfaf864b6d10d093c761f
Jeff-Tian <Jeff-Tian@users.noreply.github.com>
2024-01-10 10:36:24 +0000

    deploy: abd4d7e9a5ecafc1fe2cf5ec42244f62343ef7e3

--- Web/Controllers/Mail/MailController.cs
+++ Web/Controllers/Mail/MailController.cs
@@ -7,9 +7,18 @@ namespace Web.Controllers.Mail;
 [ApiController]
 public class MailController : ControllerBase
 {
+    private readonly ILogger<MailHandler> _logger;
+
+    public MailController(ILogger<MailHandler> logger)
+    {
+        _logger = logger;
+    }
+
     [HttpPost("SendEmail")]
     public async Task<string> SendEmail([FromServices] MailHandler handler, [FromBody] MailCommand command, CancellationToken cancellationToken)
     {
+        _logger.LogInformation("Received email request to {To} with {Subject}: {Body}", command.To, command.Subject, command.Body);
+
         var result = await handler.Handle(command, cancellationToken);
         return result.Match(success => "Success", error => "Error");
     }
