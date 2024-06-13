9044c5b36cb02b20ef03c44ef5b1bc5aa5d730e7
Jeff Tian <jeff.tian@outlook.com>
2024-06-13 12:02:44 +0800

    feat(CHN-236): add extension method

---
+++ Web/Extensions/HttpRequestMessageExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+
+namespace Web;
+
+public static class HttpRequestMessageExtensions
+{
+    public async static Task<string> ToCurlCommand(this HttpRequestMessage request)
+    {
+        var command = new StringBuilder();
+        command.Append("curl ");
+        command.Append($"-X {request.Method.Method} ");
+        command.Append($"\"{request.RequestUri}\" ");
+
+        foreach (var header in request.Headers)
+        {
+            command.Append($"-H \"{header.Key}: {string.Join(", ", header.Value)}\" ");
+        }
+
+        if (request.Content is not null)
+        {
+            var contentType = request.Content.Headers.ContentType?.MediaType;
+            if (contentType is not null)
+            {
+                command.Append($"-H \"Content-Type: {contentType}\" ");
+            }
+
+            var content = await request.Content.ReadAsStringAsync();
+
+            command.Append($"-d '{content}' ");
+        }
+
+        return command.ToString();
+    }
+}
